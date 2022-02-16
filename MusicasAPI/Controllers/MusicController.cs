1a06127b52f1438c93057f40b398b03ba9dcfff1
gabrielthinassi <gabriel.m.thinassi@gmail.com>
2022-02-16 13:52:20 -0300

    Commit English translate

---
+++ MusicasAPI/Controllers/MusicController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicasAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicController : ControllerBase
+    {
+        private static List<Music> musics = new List<Music>();
+
+        [HttpPost]
+        public void Save(Music music)
+        {
+            musics.Add(music);
+
+        }
+    }
+}
