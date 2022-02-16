badd6b02456cfc430b608020b8f0570c8a0039d0
gabrielthinassi <35746588+gabrielthinassi@users.noreply.github.com>
2022-02-16 13:46:21 -0300

    First Commit

---
+++ MusicasAPI/Controllers/MusicaController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MusicasAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicasAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MusicaController : ControllerBase
+    {
+        private static List<Musica> musicas = new List<Musica>();
+
+        [HttpPost]
+        public void Save(Musica musica)
+        {
+            musicas.Add(musica);
+
+        }
+    }
+}
---
+++ MusicasAPI/Models/Musica.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MusicasAPI.Models
+{
+    public class Musica
+    {
+        public string Title { get; set; }
+        public string Artist { get; set; }
+        public string Album { get; set; }
+        public string Gender { get; set; }
+        public int Duration { get; set; }
+    }
+}
